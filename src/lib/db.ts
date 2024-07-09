5d4ef661367ce6e1a0dd3dcd5c43f8ad68da7b1b
devalpha18 <devalpha18@gmail.com>
2024-07-09 22:26:40 +0900

    fix: migration fields type chage

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -18,8 +18,8 @@ export const migrate = async () => {
         CREATE TABLE IF NOT EXISTS users (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           email VARCHAR(255) UNIQUE NOT NULL,
-          name BOOL NOT NULL,
-          password DOUBLE NOT NULL
+          name VARCHAR(255) NOT NULL,
+          password VARCHAR(255) NOT NULL
         );
       `
   );
