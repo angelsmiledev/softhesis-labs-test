7e9246ea28e549e5e90d7c386133ff8dc080da2c
devalpha18 <devalpha18@gmail.com>
2024-07-09 21:17:58 +0900

    feat: migrate operation on db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,27 +1,45 @@
+import path from "path";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-async function openDb() {
+const dbPath = path.join(process.cwd(), "softhesis-labs.sqlite");
+
+const openDb = async () => {
   return open({
-    filename: "./mydb.sqlite",
+    filename: dbPath,
     driver: sqlite3.Database,
   });
-}
+};
 
-export async function getUser(email: string) {
+export const migrate = async () => {
+  const db = await openDb();
+  db.serialize();
+  await db.run(
+    `
+        CREATE TABLE IF NOT EXISTS users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          email VARCHAR(255) UNIQUE NOT NULL,
+          name BOOL NOT NULL,
+          password DOUBLE NOT NULL
+        );
+      `
+  );
+};
+
+export const getUser = async (email: string) => {
   const db = await openDb();
   return db.get("SELECT * FROM users WHERE email = ?", [email]);
-}
+};
 
-export async function updateUser(
+export const updateUser = async (
   email: string,
   name: string,
   password: string
-) {
+) => {
   const db = await openDb();
   await db.run("UPDATE users SET name = ?, password = ? WHERE email = ?", [
     name,
     password,
     email,
   ]);
-}
+};
